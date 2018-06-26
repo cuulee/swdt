f1635633d716a664d51937d8b2d4d94d6b18c781
Marc Becker <marcbecker@posteo.de>
2018-06-26 15:29:15 +0200

    Add opacity control to map with Leaflet.OpacityControls javascript library

--- swdt/module_tabWaterExtent.R
+++ swdt/module_tabWaterExtent.R
@@ -242,6 +242,17 @@ tabWaterExtent <- function(input,
         group = "Classified",
         opacity = 1
       ) %>%
+      onRender("function(el,x,data){
+                var map = this;
+               var labels = map.layerManager._byGroup.Classified;
+               var opacitySlider = new L.Control.opacitySlider();
+               
+               
+               for (const prop in labels) {
+               opacitySlider.setOpacityLayer(labels[prop]);
+               }
+               
+               map.addControl(opacitySlider);}") %>%
       addLegend(
         position = "topright",
         pal = pal, values = c(0, 1),
